7fb779677f1f63aaf6b13b96632a07d71c1b312b
ching163 <chinchingauyeung@gmail.com>
2026-02-09 16:49:20 +0800

    fix: update station coordinates

--- app/page.tsx
+++ app/page.tsx
@@ -12,124 +12,143 @@ import { MTR_DATA } from "@/constants/mtr";
 // so that each code in MTR_STATION roughly aligns with its station circle.
 // If you tweak or replace the map image, you may need to adjust these.
 const STATION_COORDINATES: Record<string, { x: number; y: number }> = {
-  // Airport Express (AEL)
-  HOK: { x: 52, y: 77 }, // Hong Kong
-  KOW: { x: 48, y: 69 }, // Kowloon
-  TSY: { x: 33, y: 72 }, // Tsing Yi
-  AIR: { x: 16, y: 79 }, // Airport
-  AWE: { x: 11, y: 72 }, // AsiaWorld Expo
+  // --- Airport Express (AEL) & Tung Chung Line (TCL) ---
+  HOK: { x: 42, y: 72 }, // Hong Kong
+  KOW: { x: 36, y: 66 }, // Kowloon
+  OLY: { x: 37, y: 58 }, // Olympic
+  NAC: { x: 36, y: 50 }, // Nam Cheong (Interchange TML)
+  LAK: { x: 32, y: 44 }, // Lai King (Interchange TWL)
+  TSY: { x: 28, y: 50 }, // Tsing Yi
+  SUN: { x: 23, y: 58 }, // Sunny Bay
+  TUC: { x: 17, y: 66 }, // Tung Chung
+  AIR: { x: 14, y: 62 }, // Airport
+  AWE: { x: 12, y: 56 }, // AsiaWorld-Expo
 
-  // Tung Chung Line (TCL)
-  OLY: { x: 44, y: 63 },
-  NAC: { x: 41, y: 63 },
-  LAK: { x: 37, y: 60 },
-  SUN: { x: 24, y: 77 },
-  TUC: { x: 19, y: 82 },
+  // --- Tuen Ma Line (TML) ---
+  // West / Tuen Mun section
+  TUM: { x: 10, y: 36 }, // Tuen Mun
+  SIH: { x: 10, y: 26 }, // Siu Hong
+  TIS: { x: 13, y: 17 }, // Tin Shui Wai
+  LOP: { x: 17, y: 22 }, // Long Ping
+  YUL: { x: 17, y: 27 }, // Yuen Long
+  KSR: { x: 17, y: 32 }, // Kam Sheung Road
+  TWW: { x: 28, y: 40 }, // Tsuen Wan West
+  MEF: { x: 37, y: 44 }, // Mei Foo (Interchange TWL)
+  // NAC defined above
+  AUS: { x: 46, y: 66 }, // Austin
+  ETS: { x: 52, y: 71 }, // East Tsim Sha Tsui
+  HUH: { x: 58, y: 68 }, // Hung Hom (Interchange EAL)
+  HOM: { x: 62, y: 62 }, // Ho Man Tin (Interchange KTL)
+  TKW: { x: 69, y: 59 }, // To Kwa Wan
+  SUW: { x: 71, y: 54 }, // Sung Wong Toi
+  KAT: { x: 73, y: 48 }, // Kai Tak
+  DIH: { x: 69, y: 42 }, // Diamond Hill (Interchange KTL)
+  HIK: { x: 65, y: 37 }, // Hin Keng
+  TAW: { x: 60, y: 37 }, // Tai Wai (Interchange EAL)
+  // East / Ma On Shan section
+  CKT: { x: 67, y: 31 }, // Che Kung Temple
+  STW: { x: 67, y: 26 }, // Sha Tin Wai
+  CIO: { x: 71, y: 21 }, // City One
+  SHM: { x: 75, y: 21 }, // Shek Mun
+  TSH: { x: 79, y: 21 }, // Tai Shui Hang
+  HEO: { x: 83, y: 21 }, // Heng On
+  MOS: { x: 87, y: 21 }, // Ma On Shan
+  WKS: { x: 91, y: 21 }, // Wu Kai Sha
 
-  // Tuen Ma Line (TML) – New Territories east to Tuen Mun west
-  WKS: { x: 63, y: 22 }, // Wu Kai Sha
-  MOS: { x: 60, y: 24 },
-  HEO: { x: 58, y: 26 },
-  TSH: { x: 56, y: 28 },
-  SHM: { x: 54, y: 30 },
-  CIO: { x: 52, y: 32 },
-  STW: { x: 50, y: 34 },
-  CKT: { x: 48, y: 36 },
-  TAW: { x: 46, y: 38 }, // Tai Wai (interchange)
-  HIK: { x: 44, y: 40 },
-  DIH: { x: 59, y: 46 }, // Diamond Hill (interchange with KTL)
-  KAT: { x: 64, y: 47 },
-  SUW: { x: 69, y: 47 },
-  TKW: { x: 73, y: 47 },
-  HOM: { x: 70, y: 56 }, // Ho Man Tin (interchange)
-  HUH: { x: 67, y: 64 }, // Hung Hom (interchange)
-  ETS: { x: 59, y: 69 },
-  AUS: { x: 55, y: 67 },
-  MEF: { x: 33, y: 54 }, // also on TWL
-  TWW: { x: 28, y: 52 },
-  KSR: { x: 23, y: 49 },
-  YUL: { x: 19, y: 47 },
-  LOP: { x: 16, y: 45 },
-  TIS: { x: 14, y: 43 },
-  SIH: { x: 12, y: 41 },
-  TUM: { x: 10, y: 38 },
+  // --- East Rail Line (EAL) ---
+  ADM: { x: 48, y: 78 }, // Admiralty (Terminus)
+  EXC: { x: 53, y: 75 }, // Exhibition Centre
+  // HUH defined above
+  MKK: { x: 58, y: 54 }, // Mong Kok East
+  KOT: { x: 58, y: 42 }, // Kowloon Tong (Interchange KTL)
+  // TAW defined above
+  SHT: { x: 60, y: 31 }, // Sha Tin
+  FOT: { x: 60, y: 26 }, // Fo Tan
+  RAC: { x: 63, y: 25 }, // Racecourse
+  UNI: { x: 58, y: 20 }, // University
+  TAP: { x: 53, y: 20 }, // Tai Po Market
+  TWO: { x: 48, y: 20 }, // Tai Wo
+  FAN: { x: 43, y: 20 }, // Fanling
+  SHS: { x: 39, y: 17 }, // Sheung Shui
+  LOW: { x: 35, y: 15 }, // Lo Wu
+  LMC: { x: 32, y: 22 }, // Lok Ma Chau
 
-  // Tseung Kwan O Line (TKL) – right‑hand peninsula
-  NOP: { x: 66, y: 82 }, // North Point (also Island Line)
-  QUB: { x: 70, y: 82 }, // Quarry Bay (also Island Line)
-  YAT: { x: 79, y: 69 }, // Yau Tong (also KTL)
-  TIK: { x: 83, y: 67 }, // Tiu Keng Leng (also KTL)
-  TKO: { x: 88, y: 64 }, // Tseung Kwan O
-  LHP: { x: 93, y: 63 }, // LOHAS Park
-  HAH: { x: 89, y: 58 }, // Hang Hau
-  POA: { x: 91, y: 53 }, // Po Lam
+  // --- Tsuen Wan Line (TWL) ---
+  CEN: { x: 44, y: 78 }, // Central
+  // ADM defined above
+  TST: { x: 52, y: 71 }, // Tsim Sha Tsui
+  JOR: { x: 52, y: 66 }, // Jordan
+  YMT: { x: 52, y: 60 }, // Yau Ma Tei
+  MOK: { x: 52, y: 54 }, // Mong Kok
+  PRE: { x: 52, y: 48 }, // Prince Edward
+  SSP: { x: 47, y: 44 }, // Sham Shui Po
+  CSW: { x: 43, y: 44 }, // Cheung Sha Wan
+  LCK: { x: 40, y: 44 }, // Lai Chi Kok
+  // MEF defined above
+  // LAK defined above
+  KWF: { x: 28, y: 44 }, // Kwai Fong
+  KWH: { x: 24, y: 44 }, // Kwai Hing
+  TWH: { x: 20, y: 44 }, // Tai Wo Hau
+  TSW: { x: 16, y: 44 }, // Tsuen Wan
 
-  // East Rail Line (EAL) – vertical spine to the north
-  ADM: { x: 56, y: 77 }, // Admiralty (many interchanges)
-  EXC: { x: 58, y: 74 },
-  // HUH already defined on TML
-  MKK: { x: 57, y: 55 }, // Mong Kok East
-  KOT: { x: 56, y: 44 }, // Kowloon Tong (interchange)
-  SHT: { x: 54, y: 39 },
-  FOT: { x: 54, y: 35 },
-  RAC: { x: 54, y: 31 },
-  UNI: { x: 54, y: 27 },
-  TAP: { x: 54, y: 24 },
-  TWO: { x: 54, y: 21 },
-  FAN: { x: 54, y: 18 },
-  SHS: { x: 54, y: 15 },
-  LOW: { x: 54, y: 11 },
-  LMC: { x: 58, y: 11 },
+  // --- Kwun Tong Line (KTL) ---
+  WHA: { x: 64, y: 64 }, // Whampoa
+  // HOM defined above
+  // YMT defined above
+  // MOK defined above
+  // PRE defined above
+  SKM: { x: 55, y: 46 }, // Shek Kip Mei
+  // KOT defined above
+  LOF: { x: 62, y: 42 }, // Lok Fu
+  WTS: { x: 65, y: 42 }, // Wong Tai Sin
+  // DIH defined above
+  CHH: { x: 75, y: 42 }, // Choi Hung
+  KOB: { x: 80, y: 45 }, // Kowloon Bay
+  NTK: { x: 80, y: 50 }, // Ngau Tau Kok
+  KWT: { x: 80, y: 56 }, // Kwun Tong
+  LAT: { x: 80, y: 62 }, // Lam Tin
+  YAT: { x: 82, y: 68 }, // Yau Tong
+  TIK: { x: 86, y: 68 }, // Tiu Keng Leng
 
-  // South Island Line (SIL) – bottom‑left island branch
-  OCP: { x: 39, y: 86 },
-  WCH: { x: 35, y: 88 },
-  LET: { x: 32, y: 90 },
-  SOH: { x: 30, y: 93 },
+  // --- Tseung Kwan O Line (TKL) ---
+  NOP: { x: 71, y: 79 }, // North Point
+  QUB: { x: 76, y: 79 }, // Quarry Bay
+  // YAT defined above
+  // TIK defined above
+  TKO: { x: 90, y: 64 }, // Tseung Kwan O
+  LHP: { x: 93, y: 69 }, // LOHAS Park
+  HAH: { x: 93, y: 58 }, // Hang Hau
+  POA: { x: 93, y: 52 }, // Po Lam
 
-  // Tsuen Wan Line (TWL) – red line across middle
-  CEN: { x: 49, y: 78 }, // Central
-  TST: { x: 53, y: 73 },
-  JOR: { x: 54, y: 70 },
-  YMT: { x: 55, y: 66 },
-  MOK: { x: 55, y: 61 },
-  PRE: { x: 55, y: 57 },
-  SSP: { x: 47, y: 56 },
-  CSW: { x: 42, y: 56 },
-  LCK: { x: 38, y: 56 },
-  KWF: { x: 34, y: 56 },
-  KWH: { x: 31, y: 56 },
-  TWH: { x: 28, y: 56 },
-  TSW: { x: 24, y: 56 },
+  // --- Island Line (ISL) ---
+  KET: { x: 23, y: 79 }, // Kennedy Town
+  HKU: { x: 28, y: 79 }, // HKU
+  SYP: { x: 32, y: 79 }, // Sai Ying Pun
+  SHW: { x: 38, y: 79 }, // Sheung Wan
+  // CEN defined above
+  // ADM defined above
+  WAC: { x: 53, y: 79 }, // Wan Chai
+  CAB: { x: 58, y: 79 }, // Causeway Bay
+  TIH: { x: 62, y: 79 }, // Tin Hau
+  FOH: { x: 66, y: 79 }, // Fortress Hill
+  // NOP defined above
+  // QUB defined above
+  TAK: { x: 81, y: 79 }, // Tai Koo
+  SWH: { x: 85, y: 79 }, // Sai Wan Ho
+  SKW: { x: 88, y: 84 }, // Shau Kei Wan
+  HFC: { x: 90, y: 89 }, // Heng Fa Chuen
+  CHW: { x: 92, y: 95 }, // Chai Wan
 
-  // Island Line (ISL) – bottom central blue line
-  KET: { x: 37, y: 79 },
-  HKU: { x: 40, y: 79 },
-  SYP: { x: 43, y: 79 },
-  SHW: { x: 46, y: 79 },
-  WAC: { x: 59, y: 78 },
-  CAB: { x: 63, y: 78 },
-  TIH: { x: 67, y: 78 },
-  FOH: { x: 70, y: 78 },
-  TAK: { x: 73, y: 78 },
-  SWH: { x: 76, y: 78 },
-  SKW: { x: 79, y: 78 },
-  HFC: { x: 82, y: 78 },
-  CHW: { x: 86, y: 78 },
+  // --- South Island Line (SIL) ---
+  // ADM defined above
+  OCP: { x: 53, y: 85 }, // Ocean Park
+  WCH: { x: 44, y: 91 }, // Wong Chuk Hang
+  LET: { x: 38, y: 95 }, // Lei Tung
+  SOH: { x: 34, y: 95 }, // South Horizons
 
-  // Kwun Tong Line (KTL) – green east‑Kowloon line
-  WHA: { x: 63, y: 70 },
-  SKM: { x: 52, y: 52 },
-  LOF: { x: 59, y: 49 },
-  WTS: { x: 61, y: 47 },
-  CHH: { x: 63, y: 45 },
-  KOB: { x: 66, y: 44 },
-  NTK: { x: 69, y: 44 },
-  KWT: { x: 72, y: 44 },
-  LAT: { x: 75, y: 44 },
-
-  // Disneyland Resort Line (DRL)
-  DIS: { x: 27, y: 82 },
+  // --- Disneyland Resort Line (DRL) ---
+  // SUN defined above
+  DIS: { x: 26, y: 66 }, // Disneyland Resort
 };
 
 
